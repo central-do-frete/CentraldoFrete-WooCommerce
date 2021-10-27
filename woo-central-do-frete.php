f34419350ce8e2a0a1cf487d6fd0f3c49d4f931d
Alexandre Custodio <xandy.alexandrec@gmail.com>
2021-10-27 12:10:19 -0300

    feat: update version 2.0.1

--- woo-central-do-frete.php
+++ woo-central-do-frete.php
@@ -5,7 +5,7 @@
  * Description: Módulo de cotações de frete da Central do Frete para WooCommerce
  * Author: Central do Frete
  * Author URI: https://centraldofrete.com
- * Version: 2.0.0
+ * Version: 2.0.1
  * License: GPLv2
  */
 
@@ -23,7 +23,7 @@ if ( ! class_exists( 'WooCommerce_CentralDoFrete_Main' ) ) :
 		 * Plugin version.
 		 * @var string
 		 */
-		const VERSION = '2.0.0';
+		const VERSION = '2.0.1';
 
 		/**
 		 * Instance of this class.
